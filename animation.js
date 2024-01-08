84c41bf153114765cff3cf80cbca50ceae0c5fe0
ziaur07 <zrzbusiness07@gmail.com>
2024-01-09 04:01:53 +0600

    custom cursor

--- animation.js
+++ animation.js
@@ -58,3 +58,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+// custom cursor
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    //cursorOutline.style.left = `${posX}px`;
+    //cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, {duration: 500, fill:"forwards"});
+
+});
